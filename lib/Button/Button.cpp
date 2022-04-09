b5146f099b7088fc8583ed197703485352e4421b
yaonur <ya_onur@hotmail.com>
2022-04-09 03:24:29 +0300

    readyto test

--- lib/Button/Button.cpp
+++ lib/Button/Button.cpp
@@ -1,7 +1,7 @@
 #include "Button.h"
 
-int p_state = 0;
-int c_state = 0;
+int p_state = 14;
+int c_state = 14;
 int is_pushed = 0;
 
 bool read_button(int button)
@@ -11,47 +11,99 @@ bool read_button(int button)
 	else
 		return false;
 }
-
-void process_button(int button, int cc = 0, int pc = 0, int mode = 0, int secondary = 85)
+void process_button(int button, int message, int button_type, int mode, int secondary)
 {
 	if (is_pushed == 0 && read_button(button))
 	{
-		Serial.print("button pushed:");
-		Serial.println(button);
+		Serial.print("button down:");
+		debugln(button);
 		if (mode == 0)
 		{
-			pc_button(button, pc, secondary);
-			is_pushed == 1;
-			p_state = button;
+			pc_button(button, message, secondary);
+			is_pushed = button;
+			if (button_type == 0)
+			{
+				p_state = button;
+			}
+		}
+		else
+		{
+			cc_button(button, message, secondary);
+			is_pushed = button;
+			if (button_type == 1)
+			{
+				c_state = button;
+			}
 		}
 	}
 }
 
-void pc_button(int button, int pc, int secondary)
+void pc_button(int button, int message, int secondary)
 {
-	if (p_state = button)
+	if (p_state == button)
 	{
 		send_bl_midi(secondary, 1);
 	}
 	else
 	{
-		send_bl_midi(pc, 0);
+		send_bl_midi(message, 0);
 	}
 }
 
-void send_bl_midi(int message, int message_type = 0)
+void cc_button(int button, int message, int secondary)
 {
-	if (!BLEMidiServer.isConnected())
+	if (c_state == button)
 	{
-		Serial.println("Not connected");
-		return;
-	}
-	if (message_type == 0)
-	{
-		BLEMidiServer.programChange(1, message);
+		send_bl_midi(secondary, 1);
 	}
 	else
 	{
-		BLEMidiServer.controlChange(1, message, 127);
+		send_bl_midi(message, 1);
+	}
+}
+void send_bl_midi(int message, int message_type)
+{
+	if (BLEMidiServer.isConnected() == 0)
+	{
+		debugln("Not connected");
+		debug("message was :");
+		debug(message);
+		if (message_type == 0)
+		{
+			debugln(" program change");
+		}
+		else
+		{
+			debugln(" control change");
+		}
+	}
+	else
+	{
+
+		if (message_type == 0)
+		{
+			debugln("sending pc");
+			debug(message);
+			BLEMidiServer.programChange(1, message);
+		}
+		else
+		{
+			debugln("sendingcc");
+			debug(message);
+			BLEMidiServer.controlChange(1, message, 127);
+		}
+	}
+}
+
+void reset_button(int r1, int r2)
+{
+	if (is_pushed == 0)
+	{
+		return;
+	}
+	delay(330);
+	if (digitalRead(r1) == HIGH && digitalRead(r2) == HIGH)
+	{
+		is_pushed = 0;
 	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,63 @@
-#include <Arduino.h>
+#include "Button.h"
+int r1 = 5;
+int r2 = 4;
+int c1 = 14;
+int c2 = 27;
+int c3 = 26;
+int c4 = 25;
+int c5 = 33;
+bool button_up_state = false;
 
-void setup() {
+// MIDI_CREATE_DEFAULT_INSTANCE();
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  BLEMidiServer.begin("espmcuMidi");
+  // MIDI.begin(MIDI_CHANNEL_OFF);
+  pinMode(c1, INPUT_PULLUP);
+  pinMode(c2, INPUT_PULLUP);
+  pinMode(c3, INPUT_PULLUP);
+  pinMode(c4, INPUT_PULLUP);
+  pinMode(c5, INPUT_PULLUP);
+  pinMode(r1, INPUT_PULLUP);
+  pinMode(r2, INPUT_PULLUP);
+  pinMode(22, OUTPUT);
+  debugln("setup complete");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (BLEMidiServer.isConnected())
+  {
+
+    digitalWrite(22, HIGH);
+  }
+  else
+  {
+    digitalWrite(22, LOW);
+  }
+
+  if (digitalRead(r1) == LOW)
+  {
+    process_button(c1, 1);
+    process_button(c2, 2);
+    process_button(c3, 3);
+    process_button(c4, 4);
+    process_button(c5, 5);
+    delay(3);
+  }
+  else if (digitalRead(r2) == LOW)
+  {
+    process_button(c1, 1, 1, 1, 64);
+    process_button(c2, 2, 1, 1, 64);
+    process_button(c3, 3, 1, 1, 64);
+    process_button(c4, 4, 1, 1, 64);
+    process_button(c5, 5, 3, 1, 5);
+    delay(3);
+  }
+  else if (digitalRead(r1) == HIGH && digitalRead(r2) == HIGH)
+  {
+    reset_button(r1,r2);
+  }
 }
