fb50fe08c1bbeaf8e59b5238342e6b49eebe4a5a
yaonur <ya_onur@hotmail.com>
2025-03-08 08:35:24 +0300

    ss

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 // #include <MIDI.h>
 #include <MIDIUSB.h>
-#define DEBUG 1
+#define DEBUG 0
 
-#if DEBUG == 0
+#if DEBUG == 1
 #define debug(x) Serial.print(x)
 #define debugln(x) Serial.println(x)
 #else
@@ -13,12 +13,9 @@ int r1 = 21;
 
 int is_pushed = false;
 
-// MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
-// MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup()
 {
-  // MIDI.begin(MIDI_CHANNEL_OFF); // Initialize MIDI, but don't listen to any channel
   // Serial1.begin(31250);
   // Serial.begin(9600);
 
@@ -30,30 +27,41 @@ void controlChange(byte control, byte value)
 {
   midiEventPacket_t event = {0x0B, 0xB0, control, value};
   MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
 }
 void programChange(byte pc)
 {
   midiEventPacket_t event = {0x0C, 0xC0, pc, 0x00};
   MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
 }
+void sendNoteOn(byte note, byte velocity)
+{
+  midiEventPacket_t event = {0x09, 0x90, note, velocity};
+  MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
+}
+
 int distance = 0;
 void loop()
 {
-  // distance = analogRead(r1);
-  // if (distance < 100)
-  // {
-  //   if (is_pushed == false)
-  //   {
-  //     controlChange(75, 127);
-  //     is_pushed = true;
-  //   }
-  // }
-  // else if (distance > 700)
-  // {
-  //   is_pushed = false;
-  // }
-  controlChange(37, 127);
-  delay(1000);
-  controlChange(38, 127);
-  delay(1000);
+  distance = analogRead(r1);
+  if (distance < 28)
+  {
+    if (is_pushed == false)
+    {
+      sendNoteOn(36, 127);
+      is_pushed = true;
+    }
+  }
+  else if (distance > 31)
+  {
+    is_pushed = false;
+  }
+  delay(10);
+  // debugln(distance);
+  // sendNoteOn(36, 127);
+  // delay(1000);
+  // sendNoteOn(36, 0);
+  // delay(1000);
 }
